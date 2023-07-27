a27ea381016333c4a0ce9e632dc135f3d3de2582
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-07-27 12:17:05 -0300

    Update: LinkedStackOfStringsWithAnArray can now manage its capacity

--- src/DataStructures/LinkedStackOfStringsWithAnArray.java
+++ src/DataStructures/LinkedStackOfStringsWithAnArray.java
@@ -9,15 +9,28 @@ public class LinkedStackOfStringsWithAnArray {
         stack = new String[capacity];
     }
 
+    private void resize(int capacity) {
+        String[] copy = new String [capacity];
+        for (int i = 0; i < count; i++)  copy[i] = stack[i];
+        stack = copy;
+    }
+
     public boolean isEmpty(){
         return count == 0;
     }
 
     public void push(String string){
+
+        if(count == stack.length){
+            resize(stack.length * 2);
+        }
+
         stack[count++] = string;
     }
 
     public String pop(){
+        stack[count] = null;
+        if(count > 0 &&  count == stack.length/4) resize(stack.length/2);
         return stack[--count];
     }
 
